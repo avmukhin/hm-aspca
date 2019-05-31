a0a367687ec0b26a0d2c288fbd22c7ade246b6d8
avmukhin <34707986+avmukhin@users.noreply.github.com>
2019-05-31 12:20:50 +0300

    New plotting

--- results.m
+++ results.m
@@ -3,7 +3,7 @@ hFig_c = figure('Name', 'Convergence');
 set(hFig_c, 'Position',[0.13 0.11 0.295 0.815])
 % hFig = figure(1);
 % set(hFig, 'Position', [x y width height])
-
+experiment = '1.png';
 % set(0, 'CurrentFigure', hFig)
 sNo = figure(10);
 set(sNo, 'Position',[0.13 0.225982532751092 0.334659090909091 0.526347975723483])
@@ -68,53 +68,76 @@ ylabel('D(x)','FontSize',20)
 set(0, 'CurrentFigure', sNo)
 
 plot(a_no_cg(:,end), 'linewidth', 1, 'color', 'k'); 
-legend('Real', 'Initial', 'Result')
-title('Без параметризации')
-saveas(sNo, 'figures/test/sNo_2.png', 'png')
+legend_no = legend('Real', 'Initial', 'Result');
+set(legend_no,...
+    'Position',[0.411373150989345 0.721615720524016 0.217808929613386 0.177583745776993],...
+    'FontSize',20);
+% title('Без параметризации')
+saveas(sNo, strcat('figures/test/sNo_', experiment), 'png')
 %     plot(sNo, Dinit, 'linewidth', 2, 'color', 'g');
 set(0, 'CurrentFigure', sPCA)
 plot(a_pca_cg(:,end), 'linewidth', 1, 'color', 'k');
-legend('Real', 'Initial', 'Result')
-title('PCA параметризация, 6 компонент')
-saveas(sPCA, 'figures/test/sPCA_2.png', 'png')
+legend_pca = legend('Real', 'Initial', 'Result');
+set(legend_pca,...
+    'Position',[0.411373150989345 0.721615720524016 0.217808929613386 0.177583745776993],...
+    'FontSize',20);
+% title('PCA параметризация, 6 компонент')
+% saveas(sPCA, 'figures/test/sPCA_2.png', 'png')
+saveas(sPCA, strcat('figures/test/sPCA_', experiment), 'png')
 %     plot(sPCA, Dinit, 'linewidth', 2, 'color', 'g');
 set(0, 'CurrentFigure', sRot)
 plot(a_rot_cg(:,ex_rot_cg), 'linewidth', 1, 'color', 'k'); 
-legend('Real', 'Initial', 'Result')
-title('Rotation - PCA параметризация, 6 компонент')
-saveas(sRot, 'figures/test/sRot_2.png', 'png')
+legend_rot = legend('Real', 'Initial', 'Result');
+set(legend_rot,...
+    'Position',[0.411373150989345 0.721615720524016 0.217808929613386 0.177583745776993],...
+    'FontSize',20);
+% title('Rotation - PCA параметризация, 6 компонент')
+% saveas(sRot, 'figures/test/sRot_2.png', 'png')
+saveas(sRot, strcat('figures/test/sRot_', experiment), 'png')
 %     plot(sRot, Dinit, 'linewidth', 2, 'color', 'g');
 set(0, 'CurrentFigure', sSwap)
 plot(a_swap_cg(:,ex_swap_cg), 'linewidth', 1, 'color', 'k'); 
-legend('Real', 'Initial', 'Result')
-title('Swap - PCA параметризация, 6 компонент')
-saveas(sSwap, 'figures/test/sSwap_2.png', 'png')
+legend_swap = legend('Real', 'Initial', 'Result');
+set(legend_swap,...
+    'Position',[0.411373150989345 0.721615720524016 0.217808929613386 0.177583745776993],...
+    'FontSize',20);
+% title('Swap - PCA параметризация, 6 компонент')
+% saveas(sSwap, 'figures/test/sSwap_2.png', 'png')
+saveas(sSwap, strcat('figures/test/sSwap_', experiment), 'png')
 %     plot(sSwap, Dinit, 'linewidth', 2, 'color', 'g');
 set(0, 'CurrentFigure', sExt)
 plot(a_ext_cg(:, ex_ext_cg), 'linewidth', 1, 'color', 'k');
-legend('Real', 'Initial', 'Result')
-title('Extension - PCA параметризация, 6-11 компонент')
-saveas(sExt, 'figures/test/sExt_2.png', 'png')
+legend_ext = legend('Real', 'Initial', 'Result');
+set(legend_ext,...
+    'Position',[0.411373150989345 0.721615720524016 0.217808929613386 0.177583745776993],...
+    'FontSize',20);
+% title('Extension - PCA параметризация, 6-11 компонент')
+% saveas(sExt, 'figures/test/sExt_2.png', 'png')
+saveas(sExt, strcat('figures/test/sExt_', experiment), 'png')
 %     plot(sExt, Dinit, 'linewidth', 2, 'color', 'g');
 
 set(0, 'CurrentFigure', hFig_c)
 set(hFig_c, 'Position',[0.13 0.11 0.295 0.815])
-semilogy(b_no_cg, 'b');
+semilogy(b_no_cg, 'b', 'linewidth',4);
 hold on;
-semilogy(b_pca_cg, 'k', 'linewidth',4);
-semilogy(b_rot_cg, 'r', 'linewidth',2);
-semilogy(b_swap_cg, 'm');
-semilogy(b_ext_cg, 'g');
+semilogy(b_pca_cg, 'k', 'linewidth',6);
+semilogy(b_rot_cg, 'r', 'linewidth',4);
+semilogy(b_swap_cg, 'm', 'linewidth',4);
+semilogy(b_ext_cg, 'g', 'linewidth',4);
 % Create xlabel
-xlabel('№ итерации');
+xlabel('№ итерации','FontSize',20);
 
 % Create ylabel
-ylabel('log Значение целевой функции');
+ylabel('log Значение целевой функции','FontSize',20);
 
 
-legend('Полная размерность', 'PCA','ROTATION', 'SWAP', 'EXTENSION');
-title('Сходимость методов для модели 2')
-saveas(hFig_c, 'figures/test/conv_2.png', 'png')
+legend1 = legend('Полная размерность', 'PCA','ROTATION', 'SWAP', 'EXTENSION');
+set(legend1,...
+    'Position',[0.652440728165433 0.584061135371179 0.187719296410421 0.25946142649199],...
+    'FontSize',20);
+% title('Сходимость методов для модели 2')
+% saveas(hFig_c, 'figures/test/conv_2.png', 'png')
+saveas(hFig_c, strcat('figures/test/conv_', experiment), 'png')
 
 
 
